884e6c6fd4a9f757ccee7d393d23127b9b0e6109
jashwant <ranajashwant24@gmail.com>
2025-10-21 08:17:03 +0530

    updated small change

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ConfettiProvider />
-          <ToastProvider />
+          <ToastProvider  />
           {children}
         </body>
       </html>
